72a9d982effe178bfa5aabd0acecef1d2081e2dc
som3guy <jmiller3346@gmail.com>
2014-09-26 15:42:49 -0500

    Update metadata.rb

--- cookbooks/icecast2/metadata.rb
+++ cookbooks/icecast2/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Ronnie Garcia, OVEA, http://www.ovea.com"
-maintainer_email "contact@ovea.com"
+maintainer       "NA"
+maintainer_email "NA"
 license          "Apache 2.0"
 description      "Installs/Configures icecast2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
